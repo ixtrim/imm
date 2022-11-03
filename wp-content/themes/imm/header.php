26d3bff3ef63106fc573acb7154281bc833d4d7e
ixtrim <contact@ixtrim.com>
2022-11-03 22:23:55 +0100

    Added IMM starter theme files.

---
+++ wp-content/themes/imm/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> <?php imm_schema_type(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+<meta name="viewport" content="width=device-width" />
+<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
