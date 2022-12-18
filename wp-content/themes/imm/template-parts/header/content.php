5ac88885d9daa11a714933b86e0c4da4f50144b8
ixtrim <contact@ixtrim.com>
2022-12-18 16:31:08 +0100

    Header - Desktop.

--- wp-content/themes/imm/template-parts/header/content.php
+++ wp-content/themes/imm/template-parts/header/content.php
@@ -1,22 +1,29 @@
+<?php 
+  $brand = get_field('brand', 'options');
+  if ( $brand['logotype'] ) {
+    $logo = $brand['logotype']['url'];
+  } else {
+    $logo = get_stylesheet_directory_uri() . 'assets/img/logo.png';
+  }
+?>
+
 <header id="header" class="d-flex align-items-center">
     <div class="container d-flex align-items-center justify-content-between">
 
-      <a href="index.html" class="logo"><img src="assets/img/logo.png" alt=""></a>
+      <a href="index.html" class="logo"><img src="<?php echo $logo; ?>" alt=""></a>
 
       <nav id="navbar" class="navbar">
-        <ul>
-          <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
-          <li><a class="nav-link scrollto" href="#about">About Us</a></li>
-          <li class="dropdown"><a class="nav-link scrollto" href="#services"><span>Services</span> <i class="bi bi-chevron-down"></i></a>
-            <ul>
-              <li><a href="./website-development.html">Website development and maintenance</a></li>
-              <li><a href="./google-business-managment.html">Google Business managment</a></li>
-              <li><a href="./reputation-managment.html">Reputation Management</a></li>
-            </ul>
-          </li>
-          <li><a class="nav-link scrollto" href="./client-support-portal.html">Client Support Portal</a></li>
-          <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
-        </ul>
+        <?php
+          wp_nav_menu(
+            array(
+              'theme_location'  => 'top-menu',
+              'menu_class'      => 'menu-wrapper',
+              'container_class' => 'primary-menu-container',
+              'items_wrap'      => '<ul id="primary-menu-list" class="%2$s">%3$s</ul>',
+              'fallback_cb'     => false,
+            )
+          );
+        ?>
         <i class="bi bi-list mobile-nav-toggle"></i>
       </nav><!-- .navbar -->
 
